545f89e5cd39fbb078b83562feb6604fc652d698
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2019-04-12 18:51:02 -0700

    encyrption working with lower case letters, wraps from 'z' to 'a'

--- cipher.rb
+++ cipher.rb
@@ -1,13 +1,24 @@
-def shift(string, number)
-  shifted_ascii_arr = []
-  input = string.split('')
-  output = ''
-  
-  input.each do |letter|
-    letter.ord == 32 ? shifted_ascii = 32 : shifted_ascii = letter.ord - number    
-    shifted_ascii_arr.push(shifted_ascii)
+def cipherize(operation, string, number)
+  output = ""
+  string.split("").each do |letter|
+    if letter.ord == 32
+      output += 32.chr
+    elsif letter.ord + number > 122
+      output += (letter.ord + number - 26).chr
+    elsif letter.ord + number < 97
+      output += (letter.ord + number + 27).chr
+    else
+      output += (letter.ord + number).chr
+    end  
   end
-
-  shifted_ascii_arr.each { |number| output += number.chr }  
   print(output)
 end
+
+puts "Would you like to encrypt or decrypt a message? "
+operation = gets.chomp
+puts "What is your message?"
+string = gets.chomp
+puts "How would you like to shift your message? "
+number = gets.chomp.to_i
+
+cipherize(operation, string, number)

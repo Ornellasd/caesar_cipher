96b0a2aeb0ae81ddc63220f9170730fe03eb51f4
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2019-04-15 10:20:23 -0700

    adds decryption

--- cipher.rb
+++ cipher.rb
@@ -1,24 +1,41 @@
 def cipherize(operation, string, number)
   output = ""
-  string.split("").each do |letter|
-    if letter.ord == 32
-      output += 32.chr
-    elsif letter.ord + number > 122
-      output += (letter.ord + number - 26).chr
-    elsif letter.ord + number < 97
-      output += (letter.ord + number + 27).chr
-    else
-      output += (letter.ord + number).chr
-    end  
+
+  if operation == "encrypt" || operation == "e"
+    string.split("").each do |letter|
+      if letter.ord == 32
+        output += 32.chr
+      elsif letter.ord + number > 122
+        output += (letter.ord + number - 26).chr
+      elsif letter.ord + number < 97
+        output += (letter.ord + number + 27).chr
+      else
+        output += (letter.ord + number).chr
+      end  
+    end
+  elsif operation == "decrypt" || operation == "d"
+    string.split("").each do |letter|
+      if letter.ord == 32
+        output += 32.chr
+      elsif letter.ord - number > 122
+        output += (letter.ord - number - 26).chr
+      elsif letter.ord - number < 97
+        output += (letter.ord - number + 26).chr
+      else
+        output += (letter.ord - number).chr
+      end
+    end
+  else
+    print "Error!"
   end
-  print(output)
+  print(output) 
 end
 
 puts "Would you like to encrypt or decrypt a message? "
 operation = gets.chomp
 puts "What is your message?"
 string = gets.chomp
-puts "How would you like to shift your message? "
+puts "How would you like to shift/unshift your message? "
 number = gets.chomp.to_i
 
 cipherize(operation, string, number)
